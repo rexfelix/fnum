63b96b03e1c19a798f96e6ad7368345ff86f50fd
rexfelix <rexfelix@naver.com>
2022-08-12 18:44:23 +0900

    fnum

--- test/speed_test.py
+++ test/speed_test.py
@@ -1,7 +1,6 @@
 import time
 from fnum import fibo, facto
 
-count = 15
 
 def facto_py(n):
     if n == 0:
@@ -21,30 +20,35 @@ def fibo_py(n):
         return fibo(n - 1) + fibo(n - 1)
 
 
-start = time.time()
-for i in range(count):
-    n = facto(i)
-    print(f"{n}", end=" ")
-end = time.time_ns()
-print(f"\nRust facto = {end - start}\n")
+def speed_test(count):
+    start = time.time()
+    for i in range(count):
+        n = facto(i)
+        print(f"{n}", end=" ")
+    end = time.time_ns()
+    print(f"\nRust facto = {end - start}\n")
 
-start = time.time()
-for i in range(count):
-    n = facto_py(i)
-    print(f"{n}", end=" ")
-end = time.time_ns()
-print(f"\npython facto = {end - start}\n")
+    start = time.time()
+    for i in range(count):
+        n = facto_py(i)
+        print(f"{n}", end=" ")
+    end = time.time_ns()
+    print(f"\npython facto = {end - start}\n")
 
-start = time.time()
-for i in range(count):
-    n = fibo(i)
-    print(f"{n}", end=" ")
-end = time.time_ns()
-print(f"\nRust fibo = {end - start}\n")
+    start = time.time()
+    for i in range(count):
+        n = fibo(i)
+        print(f"{n}", end=" ")
+    end = time.time_ns()
+    print(f"\nRust fibo = {end - start}\n")
 
-start = time.time()
-for i in range(count):
-    n = fibo_py(i)
-    print(f"{n}", end=" ")
-end = time.time_ns()
-print(f"\npython fibo = {end - start}\n")
+    start = time.time()
+    for i in range(count):
+        n = fibo_py(i)
+        print(f"{n}", end=" ")
+    end = time.time_ns()
+    print(f"\npython fibo = {end - start}\n")
+
+
+if __name__ == "__main__":
+    speed_test(15)
